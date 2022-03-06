a816e7a293596070165059fa8333a73453068acd
Caiocesar173 <caiocesar173caiocesar173@gmail.com>
2022-03-05 23:26:13 -0300

    Removing audits for improving performace

--- src/Abstracts/ModelAbstract.php
+++ src/Abstracts/ModelAbstract.php
@@ -1,14 +1,10 @@
 <?php
 namespace Caiocesar173\Utils\Abstracts;
 
-use OwenIt\Auditing\Auditable;
-use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
-
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -17,38 +13,14 @@ use CaioCesar173\Utils\Enum\StatusEnum;
 use Caiocesar173\Utils\Traits\UuidKeyTrait;
 use Caiocesar173\Utils\Abstracts\RepositoryAbstract;
 
-abstract class ModelAbstract extends Model implements Transformable, AuditableContract
+
+abstract class ModelAbstract extends Model implements Transformable
 {
-    use TransformableTrait, UuidKeyTrait, Auditable, HasFactory;
+    use TransformableTrait, UuidKeyTrait, HasFactory;
 
-    public static function create(Array $data)
-    {
-        DB::beginTransaction();
-        try {
-            $model = self::firstOrNew($data);
-            if ($model->save()) 
-            {
-                DB::commit();
-                return $model;
-            }
-            DB::rollback();
-            return 'unable to save';
-            
-        } catch (\Exception $e) {
-            DB::rollback();
-            return "ops, something went wrong... [$e]";
-        }
-    }
-
-    public static function list($identifier = null, $findBy = 'id')
-    {
-        if ( !is_null($identifier) )
-            $model = self::where($findBy, $identifier)->first();
-        else
-            $model = self::all();
-
-        return $model;
-    }
+    protected $auditHide = [];
+    protected $auditTranslate = [];
+    protected $auditTranslateValue = [];
 
     public static function edit($data, $identifier = null, $findBy = 'id')
     {
@@ -58,7 +30,7 @@ abstract class ModelAbstract extends Model implements Transformable, AuditableCo
             if (is_null($model)) 
                 return "unable to find any results, searching: ($findBy = $identifier)";
 
-            if ($model->save( $data )) 
+            if ($model->update( $data )) 
             {
                 DB::commit();
                 return $model;
@@ -98,39 +70,117 @@ abstract class ModelAbstract extends Model implements Transformable, AuditableCo
         }
     }
 
+    public function getValueModel($field, $default = '')
+    {
+        $list = explode('.', $field);
+        $model = clone $this;
+
+        foreach ($list as $position) 
+        {
+            $value = $model->{$position} ?? $default;
+            $method = str_replace(' ', '', lcfirst(ucwords('form ' . str_replace('_', ' ', $position) . ' Attribute')));
+
+            if (method_exists($model, $method)) 
+            {
+                $model = $model->{$method}($value);
+                continue;
+            }
+
+            $model = $value;
+        }
+
+        return $model ?? $default;
+    }
+
     protected static function getRepository()
     {
-        $classe = str_replace('Entities', 'Repositories', get_called_class() );
-        return app($classe.'Repository');
+        $class = str_replace('Entities', 'Repositories', get_called_class());
+        return app($class . 'Repository');
     }
 
-    protected function HasManyRepository($classe)
+    public function preSave(array $attributes): array
     {
-        $repository = $classe::getRepository();
-
-        return $this->relationshipRepository($this->hasMany($classe), $repository);
+        return $attributes;
     }
 
-    private function RelationshipRepository(Relation $relation, RepositoryAbstract $repository)
+    protected function hasManyRepository($class, $foreignKey = null, $localKey = null)
     {
-        foreach ( $repository->getCriteria() as $criteria) :
+        $reposirory = $class::getRepository();
+        return $this->relationshipRepository($this->hasMany($class, $foreignKey, $localKey), $reposirory);
+    }
+
+    protected function hasOneRepository($class)
+    {
+        $reposirory = $class::getRepository();
+        return $this->relationshipRepository($this->hasOne($class), $reposirory);
+    }
+
+    protected function morphManyRepository($related, $name, $type = null, $id = null, $localKey = null)
+    {
+        $reposirory = $related::getRepository();
+        return $this->relationshipRepository($this->morphMany($related, $name, $type, $id, $localKey), $reposirory);
+    }
+
+    protected function belongsToRepository($class)
+    {
+        $reposirory = $class::getRepository();
+        return $this->relationshipRepository($this->belongsTo($class), $reposirory);
+    }
+
+    private function relationshipRepository(Relation $relation, RepositoryAbstract $reposirory)
+    {
+        foreach ($reposirory->getCriteria() as $criteria)
+        {
             $filter = app(get_class($criteria));
-            $relation = $filter->apply($relation, $repository);
-        endforeach;
+            $relation = $filter->apply($relation, $reposirory);
+        }
 
         return $relation;
     }
 
-    public function ScopePluckMultiple($query, string $keyColumn, array $extraFileds): Collection
+    /**
+     * Scope a query to Pluck The Multiple Columns
+     *
+     * This is Used to Pluck the multiple Columns in the table based
+     * on the existing query builder instance
+     *
+     * @author Manojkiran.A <manojkiran10031998@gmail.com>
+     * @version 0.0.2
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @param string $keyColumn the columns Which is used to set the key of array
+     * @param array $extraFileds the list of columns that need to plucked in the table
+     * @return \Illuminate\Support\Collection
+     * @throws Illuminate\Database\QueryException
+     **/
+    public function scopePluckMultiple($query, string $keyColumn, array $extraFileds): \Illuminate\Support\Collection
     {
+        //pluck all the id based on the query builder instance class
         $keyColumnPluck = $query->pluck($keyColumn)->toArray();
 
-        foreach ((array)$extraFileds as  $eachFiled) 
-        {
-            $extraFields[$eachFiled] =   $query->pluck($eachFiled)->toArray();
-        }
+        //start @deprecated because slower than foreach
 
+        //anonymous callback method to iterate over the each fileds of table
+
+        // $callBcakMethod = function ($eachValue) use ($query) {
+        //     $eachQuery[$eachValue] = $query->pluck($eachValue)->toArray();
+        //     return $eachQuery;
+        // };
+        //now we are collapsing the array single time to get the propered array
+
+        // $extraFields = Arr::collapse(array_map($callBcakMethod, $extraFileds));
+
+        //end @deprecated because slower than foreach
+
+        //iterating Through All Other Fileds and Plucking it each Time
+        foreach ((array)$extraFileds as  $eachFiled)
+            $extraFields[$eachFiled] =   $query->pluck($eachFiled)->toArray();
+
+        //now we are done with plucking the Required Columns
+        //we need to map all the values to each key
+
+        //get all the keys of extra fileds and sets as array key or index
         $arrayKeys = array_keys($extraFields);
+        //get all the extra fileds array and mapping it to eack key
         $arrayValues = array_map(
             function ($value) use ($arrayKeys) {
                 return array_combine($arrayKeys, $value);
@@ -142,7 +192,12 @@ abstract class ModelAbstract extends Model implements Transformable, AuditableCo
                 $extraFields
             ))
         );
+        //now we are done with the array now Convert it to Collection
+        return new \Illuminate\Support\Collection(array_combine($keyColumnPluck, $arrayValues));
+    }
 
-        return new Collection(array_combine($keyColumnPluck, $arrayValues));
+    public function isDestroyed()
+    {
+        return $this->status == StatusEnum::EXCLUDED;
     }
 }
